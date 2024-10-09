cefb802486cc06a38bf4f1d5cb6def832e58df22
Wallace Martins <wmvwallace@gmail.com>
2024-10-08 22:58:52 -0300

    Atividade 02 - Triggers

---
+++ Scripts/Triggers02.sql
@@ -0,0 +1,6 @@
+USE faculdade;
+
+-- CRIAR PROCEDURE QUE:
+
+-- Liste os códigos dos professores com título denominado 'Doutor' que não ministraram aulas em 2019/1.
+-- Caso nao existam professores dar uma mensagem de erro usando um dos métodos para o tratamento de Exceções.
