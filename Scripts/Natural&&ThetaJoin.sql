64dd3c1498c37dd79a5db44784588f6eba54510a
Wallace Martins <wmvwallace@gmail.com>
2024-10-15 11:35:19 -0300

    Atividade Natural e Theta-Join

---
+++ Scripts/Natural&&ThetaJoin.sql
@@ -0,0 +1,5 @@
+USE faculdade;
+
+-- 1- Obter os nomes dos departamentos que têm turmas que, em 2023/1, têm aulas na sala 203 do prédio denominado 'Informática - aulas'.
+-- Resolver usando theta-join e junção natural.
+
